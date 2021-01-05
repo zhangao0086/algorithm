d140a821d27362bf3bd5e6bdc3515ecbce50ea72
Bannings <zhangao0086@gmail.com>
2021-01-05 13:28:15 +0800

    update

--- find_first_and_last_position_of_element_in_sorted_array.py
+++ find_first_and_last_position_of_element_in_sorted_array.py
@@ -3,6 +3,8 @@
 
 __author__ = "Bannings"
 
+from typing import List
+
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if len(nums) == 0: return [-1, -1]
