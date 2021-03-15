96695b5463e26c1758ae8ee8eb0bd2d39834374c
Bannings <zhangao0086@gmail.com>
2021-03-15 11:34:40 +0800

    update

---
+++ checkio/roman-numerals.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*-coding:utf-8-*-
+
+__author__ = "Bannings"
+
+def checkio(data):
+    M = ["", "M", "MM", "MMM"]
+    C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+    return M[data // 1000] + C[data % 1000 // 100] + X[data % 100 // 10] + I[data % 10]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
+    print('Done! Go Check!')
