5a58f2b6b3c409c5bb420eae755a2bcae72bc557
zhangao0086 <zhangao0086@gmail.com>
2022-11-18 19:04:25 +0800

    ugly-number

---
+++ leetcode/ugly-number/src/main.rs
@@ -0,0 +1,27 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_ugly(n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+        let mut n = n;
+        while n % 2 == 0 {
+            n /= 2;
+        }
+        while n % 3 == 0 {
+            n /= 3;
+        }
+        while n % 5 == 0 {
+            n /= 5;
+        }
+        n == 1
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::is_ugly(6), true);
+    assert_eq!(Solution::is_ugly(8), true);
+    assert_eq!(Solution::is_ugly(14), false);
+    assert_eq!(Solution::is_ugly(1), true);
+}
